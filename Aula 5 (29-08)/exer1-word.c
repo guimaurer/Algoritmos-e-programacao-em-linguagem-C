1a58d3351db959d14bb7eeeea46ca637619a12c2
Guilherme Maurer <guilhermemaurer@hotmail.com>
2022-08-29 14:29:23 +0000

    Exercício 2 feito do word feito

--- Aula 5 (29-08)/exer1-word.c
+++ Aula 5 (29-08)/exer1-word.c
@@ -15,25 +15,30 @@ int main() {
   float val_compra;
 
   printf("\nDigite o nivel de usuario:\n1 - Usuario comum  \n2 - "
-         "Funcionario\n3 - VIP\n");
+"Funcionario\n3 - VIP\n");
   scanf("%d", &tipo_user);
 
   if (tipo_user > 0 && tipo_user < 4) {
     printf("\nDigite o valor total da sua conta: ");
     scanf("%f", &val_compra);
 
-    switch (tipo_user) {
-    case 1:
-      printf("\nTotal da sua compra é %.2f", val_compra);
-      break;
+    if (val_compra > 0) {
 
-    case 2:
-      printf("\nTotal da sua compra é %.2f", val_compra * 0.10 - val_compra);
-      break;
+      switch (tipo_user) {
+      case 1:
+        printf("\nTotal da sua compra é %.2f", val_compra);
+        break;
 
-    case 3:
-      printf("\nTotal da sua compra é %.2f", val_compra * 0.05 - val_compra);
-      break;
+      case 2:
+        printf("\nTotal da sua compra é %.2f", val_compra * 0.9);
+        break;
+
+      case 3:
+        printf("\nTotal da sua compra é %.2f", val_compra * 0.95);
+        break;
+      }
+    } else {
+      printf("voce digitou um valor menor que 0");
     }
 
   } else {
---
+++ exer2-mes-word.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+Escreva um programa utilizando o comando switch que imprima um mês de acordo
+com o número digitado pelo usuário.
+
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main() {
+  int mes;
+  printf("\nDigite o um número do mes: ");
+  scanf("%d", &mes);
+
+  switch (mes) {
+  case 1:
+    printf("\nSeu mes é Janeiro");
+    break;
+
+  case 2:
+    printf("\nSeu mes é Fevereiro");
+    break;
+
+  case 3:
+    printf("\nSeu mes é Marco");
+    break;
+
+  case 4:
+    printf("\nSeu mes é Abril");
+    break;
+
+  case 5:
+    printf("\nSeu mes é Maio");
+    break;
+
+  case 6:
+    printf("\nSeu mes é Junho");
+    break;
+
+  case 7:
+    printf("\nSeu mes é Julho");
+    break;
+
+  case 8:
+    printf("\nSeu mes é Agosto");
+    break;
+
+  case 9:
+    printf("\nSeu mes é Setembro");
+    break;
+
+  case 10:
+    printf("\nSeu mes é Outubro");
+    break;
+
+  case 11:
+    printf("\nSeu mes é Novembro");
+    break;
+
+  case 12:
+    printf("\nSeu mes é Dezembro");
+    break;
+
+  default:
+    printf("\nVoce digitou um mes invalido");
+    main();
+    break;
+  }
+}
